f4078ca7fb67997f36bd1e1cb6dd33a27ddd3b7a
Đai ca Anh <ngocanhvu18ict@gmail.com>
2018-04-23 08:48:27 +0700

    Initial Commit

---
+++ DownLoadDislayView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DownLoadDislayView
+//
+//  Created by NgocAnh on 4/23/18.
+//  Copyright © 2018 NgocAnh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
